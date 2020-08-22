a9cb4e629b431b1714b0a83ddc228525ad49b353
Jake Creps <crepschrome@gmail.com>
2020-08-22 14:24:18 -0400

    Create snapsearch.py
    Initial code.

---
+++ snapsearch.py
@@ -0,0 +1,22 @@
+import webbrowser
+import requests
+import pyfiglet
+from colorama import Fore, init
+init(autoreset=True)
+
+banner = pyfiglet.figlet_format("SnapSearch")
+print(Fore.YELLOW + banner)
+username = input("Enter a username: ")
+check_snap = "https://feelinsonice.appspot.com/web/deeplink/snapcode?username={}&size=400&type=SVG".format(username)
+verify_snap = "https://snapchat.com/add/{}".format(username)
+r = requests.get(check_snap)
+
+if "image clip" in r.text:
+    print(Fore.YELLOW + "[!] Snapchat Exists!")
+    verify = input("Do you want to verify? " + Fore.RED + "(y/n) ")
+    if verify == "y":
+        webbrowser.open(verify_snap)
+    else:
+        exit()
+else:
+    print("Snapchat Doesn't Exist!")
